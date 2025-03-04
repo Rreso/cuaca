c366e95f6fbac413d6a40681123f4fff7ab955ae
Rreso <143344200+Rreso@users.noreply.github.com>
2025-03-04 21:25:19 +0700

    Update cuacaiot1.py

--- cuacaiot1.py
+++ cuacaiot1.py
@@ -67,7 +67,7 @@ else:
                            value=(min_date, max_date))
 
     # 🔹 Konversi kembali ke datetime untuk filtering
-    start_date, end_date = pd.to_datetime(date_range[0]), pd.to_datetime(date_range[10])
+    start_date, end_date = pd.to_datetime(date_range[0]), pd.to_datetime(date_range[1])
     df_filtered = df.loc[start_date:end_date]
     
     # 🔹 Tampilkan tabel data
